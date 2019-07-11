6543ae205fc596a4578e72144521cfabecaff57b
Android2007 <52743561+Android2007@users.noreply.github.com>
2019-07-11 18:40:49 +0700

    Chastota

---
+++ get_freq.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define B_S 300
+
+// ENCODER
+void get_freq (char *buffer,int dlina){
+int freq[257];
+for (int i=0;i<257;i++){
+    freq[i] = 0;
+}
+
+
+for (int i=0;i<dlina;i++){
+freq[buffer[i]] += 1;
+}
+printf("%d",freq['a']);
+}
+// -------
+
+
+
+
+
+
+int main(){
+    FILE *input = fopen("C:\\C\\File1.txt","r");
+    char buffer[B_S];
+    int char_read = fread(buffer, sizeof(char),B_S,input);
+    get_freq(buffer,char_read);
+}
