271ff9c3cb0e8de0068073ac11528dfefa55e9f9
Android2007 <52743561+Android2007@users.noreply.github.com>
2019-07-12 17:58:31 +0700

    Delete tree_get_char.h

--- tree_get_char.h
+++
@@ -1,6 +0,0 @@
-#ifndef ENCODER_H_INCLUDED
-#define ENCODER_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-char tree_get_char(ctree_node *derevo,bitstream *logic)
-#endif // ENCODER_H_INCLUDED
