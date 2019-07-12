1d672ce6df2f46b74522a3ad2256187bef5c59f9
Arachezy <nik@orlenko.com>
2019-07-12 17:03:04 +0700

    Vikagr patch 1 (#6)
    * Сreated ctree_free_tree
    
    this function delets tree
    
    * coding_tree
    
    node for tree
    
    * coding tree
    
    new_node
    
    * rebuild
    
    rebuild_tree
    
    * coding_tree
    
    coding_tree
    
    * coding_tree_c
    
    int letter
    
    * coding tree
    
    * decode
    
    more decode

--- coding_tree.c
+++ coding_tree.c
@@ -1,24 +1,26 @@
 #include "coding_tree.h"
 
+typedef st_ctree_node ctree_node;
+struct st_ctree_node
+{
+    int letter;
+    unsigned int freq;
+    ctree_node children[2];
+};
 void ctree_free_tree (ctree_node *a)
 {
 	if (a -> children[0] != NULL)
 	{
-		del_tree (a->children[0]);
+
+		ctree_free_tree (a->children[0]);
 	}
 	if (a -> children[1] != NULL)
 	{
-		del_tree (a->children[1]);
+		ctree_free_tree (a->children[1]);
 	}
 	free (a);
 }
-typedef st_ctree_node ctree_node;
-struct st_ctree_node
-{
-    char letter;
-    unsigned int freq;
-    ctree_node children[2];
-};
+
 ctree_node *new_ctree_node (unsigned int freq, char letter, *child[0], *child[1])
 {
 	ctree_node *nov = (ctree_node *) malloc(sizeof(ctree_node));
