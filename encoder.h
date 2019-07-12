01e789106a42b5884a084aca8af345189ef8b99d
vikagr <52743408+vikagr@users.noreply.github.com>
2019-07-12 17:59:22 +0700

    Add files via upload

---
+++ encoder.h
@@ -0,0 +1,9 @@
+#ifndef ENCODER_H_INCLUDED
+#define ENCODER_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+void get_freq (FILE *input);
+encode(char *in,char *ou);
+
+
+#endif // ENCODER_H_INCLUDED
