62a38381bd9bc9c947c861678c16fe4478f8d8cc
Android2007 <52743561+Android2007@users.noreply.github.com>
2019-07-12 14:42:22 +0700

    Update get_freq.c

--- get_freq.c
+++ get_freq.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define B_S 300
 
-// ENCODER
-void get_freq (char *buffer,int dlina,FILE *input){
+void get_freq (char *buffer,int size,FILE *input){
 int freq[257];
 for (int i=0;i<257;i++){
     freq[i] = 0;
 }
 
-
-for (int i=0;i<dlina;i++){
+for (int i=0;i<size;i++){
 freq[buffer[i]] += 1;
 }
 rewind(FILE*input)
 return freq;
 }
-// -------
-
-/*
-int main(){
-    FILE *input = fopen("C:\\C\\File1.txt","r");
-    char buffer[B_S];
-    int char_read = fread(buffer, sizeof(char),B_S,input);
-    get_freq(buffer,char_read);
-}
-*/
