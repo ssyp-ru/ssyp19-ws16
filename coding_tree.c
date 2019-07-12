0b57ddf8ea6aa5cec0956da4e43d831dff726c9f
vikagr <52743408+vikagr@users.noreply.github.com>
2019-07-12 12:53:08 +0700

    coding_tree_c
    int letter

--- coding_tree.c
+++ coding_tree.c
@@ -1,5 +1,11 @@
 #include "coding_tree.h"
-
+typedef st_ctree_node ctree_node;
+struct st_ctree_node
+{
+    int letter;
+    unsigned int freq;
+    ctree_node children[2];
+};
 void ctree_free_tree (ctree_node *a)
 {
 	if (a -> children[0] != NULL)
@@ -12,13 +18,6 @@ void ctree_free_tree (ctree_node *a)
 	}
 	free (a);
 }
-typedef st_ctree_node ctree_node;
-struct st_ctree_node
-{
-    char letter;
-    unsigned int freq;
-    ctree_node children[2];
-};
 ctree_node *new_ctree_node (unsigned int freq, char letter, *child[0], *child[1])
 {
 	ctree_node *nov = (ctree_node *) malloc(sizeof(ctree_node));
