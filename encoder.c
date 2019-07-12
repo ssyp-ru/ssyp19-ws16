930b8f77be83c9a935b2f23ddf1ae8054d51c8c1
cheegoo <52743384+cheegoo@users.noreply.github.com>
2019-07-12 18:08:07 +0700

    Update encoder.c

--- encoder.c
+++ encoder.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+typedef st_ctree_node ctree_node;
+typedef st_code code;
+struct st_code
+{
+	int size;
+	char code[257];
+};
+struct st_ctree_node
+{
+	char letter;
+	unsigned int freq;
+	ctree_node *children[2];
+};
 void get_freq (FILE *input){
 char buffer[257];
 int size = fread(buffer, sizeof(char),257,input)
@@ -26,4 +38,38 @@ encode(char *in,char *ou){
             code_char(i,ctree_build_tree(get_freq(input)),bitstream*);
         }
     }
+}
+ctree_node* huf_tree_build(FILE* file)
+{
+	ctree_node* tree;
+	tree = ctree_build_tree(get_freq(file));
+	return tree;
+}
+ctree_node* calc_codes(ctree_node* tree, int length, code* symbol)
+{
+	if (tree->child[0] != NULL)
+	{
+		symbol[length] = 0;
+		code_char(tree->children[0], length + 1);
+	}
+	if (tree->child[1] != NULL)
+	{
+		symbol[length] = 1;
+		code_char(tree->children[1], length + 1);
+	}
+	if ((tree->child[1] == NULL) && (tree->child[0] == NULL))
+	{
+		for(int index = 0; index < length; index++)
+		{
+			symbol->code[index] = symbol[index];
+		}
+	}
+}
+ctree_node* code_char (bitstream* b_stream, code* symbol)
+{
+	bitstream* bs;
+	for (int i = 0; i < 257; i++)
+	{
+		bitstream_put_bit(symbol->code[i], bs);
+	}
 }
