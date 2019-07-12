e0fa681d9369bbf8de26d7667ca39c803480c8d1
cheegoo <52743384+cheegoo@users.noreply.github.com>
2019-07-12 18:40:56 +0700

    Update decode.h

--- decode.h
+++ decode.h
@@ -2,6 +2,10 @@
 #define DECODE_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-void decode(FILE *from, FILE *where)
-node huf_rebuild_tree(FILE *input)
+#include coding_tree.h
+#define BUFER_SIZE = 257
+char tree_get_char(ctree_node *derevo,bitstream *logic);
+node huf_rebuild_tree(FILE *input);
+void decode(FILE *from, FILE *where);
+node huf_rebuild_tree(FILE *input);
 #endif // DECODE_H_INCLUDED
