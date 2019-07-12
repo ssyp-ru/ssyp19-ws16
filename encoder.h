34eaa2e03cb271260a384b0e7a9fb74f25a4a693
cheegoo <52743384+cheegoo@users.noreply.github.com>
2019-07-12 18:09:56 +0700

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -2,6 +2,22 @@
 #define ENCODER_H_INCLUDED
 #include <stdlib.h>
 #include <stdio.h>
+typedef st_ctree_node ctree_node;
+typedef st_code code;
+struct st_code
+{
+	int size;
+	char code[257];
+};
+struct st_ctree_node
+{
+	char letter;
+	unsigned int freq;
+	ctree_node *children[2];
+};
+ctree_node* huf_tree_build(FILE* file);
+ctree_node* calc_codes(ctree_node* tree, int length, code* symbol);
+ctree_node* code_char (bitstream* b_stream, code* symbol);
 void get_freq (FILE *input);
 encode(char *in,char *ou);
 
