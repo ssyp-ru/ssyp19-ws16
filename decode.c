7e5f38b2a5c70549af0e3800700bef23b3d966c7
Android2007 <52743561+Android2007@users.noreply.github.com>
2019-07-12 17:57:04 +0700

    Update decode.c

--- decode.c
+++ decode.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include coding_tree.h
 #define BUFER_SIZE = 257
- node huf_rebuild_tree(FILE *input)
+
+char tree_get_char(ctree_node *derevo,bitstream *logic){
+for(derevo -> children[0] =! NULL){
+    if(bitstream_get_bit(logic)) = 0){
+        derevo = derevo ->children[0];
+    }
+    else{
+        derevo = derevo ->children[1];
+    }
+}
+return derevo.letter;
+
+}
+
+node huf_rebuild_tree(FILE *input)
 {
     int read;
     char buffer [BUFER_SIZE];
