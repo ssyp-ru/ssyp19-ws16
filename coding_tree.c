9381cf39245b248c622e7f0a3c4bd10bb0971e2e
cheegoo <52743384+cheegoo@users.noreply.github.com>
2019-07-12 15:28:58 +0700

    codding_tree

--- coding_tree.c
+++ coding_tree.c
@@ -4,11 +4,11 @@ void ctree_free_tree (ctree_node *a)
 {
 	if (a -> children[0] != NULL)
 	{
-		del_tree (a->children[0]);
+		ctree_free_tree(a->children[0]);
 	}
 	if (a -> children[1] != NULL)
 	{
-		del_tree (a->children[1]);
+		ctree_free_tree(a->children[1]);
 	}
 	free (a);
 }
